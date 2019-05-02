ff5c7a284d9a3af747d041fc1c890e495af62f41
Ivan Trutnev <>
2019-05-02 16:14:42 +0300

    implement logic of create/delete/complete tasks

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -3,22 +3,28 @@ import PropTypes from 'prop-types';
 
 import './TodoItem.css';
 
-const ToDoItem = ({ text, isCompleted }) => (
+const ToDoItem = ({ text, isCompleted, removeTask, id, completeTask}) => (
   <li className="todo-item">
-    <i className={isCompleted ? 'mark far fa-check-circle' : 'mark far fa-circle'} />
+    <i className={isCompleted ? 'mark far fa-check-circle' : 'mark far fa-circle'} onClick={() => completeTask(id)}/>
     <span className={isCompleted ? 'completed text' : 'text'}>{text}</span>
-    <i className="fas fa-times" />
+    <i onClick={() => removeTask(id)} className="fas fa-times" />
   </li>
 );
 
 ToDoItem.propTypes = {
   text: PropTypes.string,
   isCompleted: PropTypes.bool,
+  removeTask: PropTypes.func,
+  id: PropTypes.number,
+  completeTask: PropTypes.func
 }
 
 ToDoItem.defaultProps = {
   text: '',
   isCompleted: false,
+  removeTask: () => {},
+  id: '',
+  completeTask: () => {},
 }
 
 export default ToDoItem;
--- src/containers/Todo/Todo.js
+++ src/containers/Todo/Todo.js
@@ -1,48 +1,52 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { addTask, removeTask, completeTask } from '../../actions/actionCreator';
 
 import ToDoInput from '../../components/TodoInput/TodoInput';
-import ToDoList from '../../components/TodoList/TodoList';
+import ToDoList from '../../components/TodoList/TodoList'; 
 import ToDoFooter from '../../components/TodoFooter/TodoFooter';
 
 import './Todo.css';
 
-const TASKS = [
-  {
-    id: 1,
-    text: 'Learn ReactJS',
-    isCompleted: true,
-  },
-  {
-    id: 2,
-    text: 'Learn Redux',
-    isCompleted: false,
-  },
-  {
-    id: 3,
-    text: 'Learn React Router',
-    isCompleted: false,
-  }
-];
-
 class ToDo extends Component {
+	state = {
+		activeFilter: 'all',
+		taskText: ''
+	};
 
-  state = {
-    activeFilter: 'all',
+	handleInputChange = ({ target: { value } }) => {
+		this.setState({
+			taskText: value
+		});
+  };
+  
+  addTask = ({ key }) => {
+    const { taskText } = this.state;
+    if(taskText.length > 3 && key === 'Enter') {
+      const { addTask } = this.props;
+      addTask(new Date().getTime(), taskText, false);
+      this.setState({
+        taskText: ''
+      })
+    }
   }
 
-  render() {
-    const { activeFilter } = this.state;
-    const tasksList = TASKS;
-    const isTasksExist = tasksList && tasksList.length > 0;
+	render() {
+		const { activeFilter, taskText } = this.state;
+		const {tasks, removeTask, completeTask} = this.props;
+		const isTasksExist = tasks && tasks.length > 0;
 
-    return (
-      <div className="todo-wrapper">
-        <ToDoInput />
-        {isTasksExist && <ToDoList tasksList={tasksList} />}
-        {isTasksExist && <ToDoFooter amount={tasksList.length} activeFilter={activeFilter} />}
-      </div>
-    );
-  }
+		return (
+			<div className="todo-wrapper">
+				<ToDoInput value={taskText} onChange={this.handleInputChange} onKeyPress={this.addTask}/>
+				{isTasksExist && <ToDoList tasksList={tasks} removeTask={removeTask} completeTask={completeTask}/>}
+				{isTasksExist && <ToDoFooter amount={tasks.length} activeFilter={activeFilter} />}
+			</div>
+		);
+	}
 }
 
-export default ToDo;
+export default connect(state => ({
+  tasks: state.tasks
+}), {addTask, removeTask, completeTask})(ToDo);
